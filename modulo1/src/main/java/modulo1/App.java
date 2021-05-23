185396b2a815da57d7cd1a7c5c281ecc8b656e49
Éderson Cássio <ederson.cassio@scipopulis.com>
2021-05-23 00:36:03 -0300

    Modularização com maven

---
+++ modulo1/src/main/java/modulo1/App.java
@@ -0,0 +1,9 @@
+package modulo1;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Módulo 1");
+	}
+
+}
---
+++ modulo2/src/main/java/modulo2/App.java
@@ -0,0 +1,9 @@
+package modulo2;
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Módulo 2");
+	}
+
+}
